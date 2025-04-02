5934c968e5f32afdcd20e395a49727628c2beab7
Kgo890 <gorekaden501@gmail.com>
2025-04-01 21:25:45 -0700

    Finished the game and added a score board

--- main.py
+++ main.py
@@ -1,18 +1,66 @@
+import sys
+
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    score = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, asteroids, drawable)
+    AsteroidField.containers = updatable
+    field = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (updatable, drawable, shots)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collision(asteroid):
+                    shot.kill()
+                    asteroid.split()
+                    score += 1
+
+                if player.collision(asteroid):
+                    print("Game over!")
+                    print(f"Your high score was {score}")
+                    pygame.quit()
+                    sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            font = pygame.font.Font(None, 36)
+            score_text = font.render(F"Score: {score}", True, "white")
+            screen.blit(score_text, (10, 10))
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
